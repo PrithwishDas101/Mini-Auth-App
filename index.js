d69e46bb40b3f6e599e7b81ac7dabb7a34b2225f
PrithwishDas101 <prithwish6636@gmail.com>
2026-03-31 23:38:46 +0530

    Initial auth project setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("mongodb connected");  
+}).catch(err => {
+    console.log(err);    
+})
+
+app.get('/', (req, res) => {
+    res.json({message:"hello from the server"});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("listening...");
+});
